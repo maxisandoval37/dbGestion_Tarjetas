035d4e4ce1fc6c5cb0548068f0d6897eb79f0c2e
TomyMontenegro <tomymontenegro1999@hotmail.com>
2020-11-24 19:23:16 -0300

    En el archivo generarDatos se agregaron datos de nombre y direcciones a cliente y algunos nombres a comercio

--- sql/generarDatos.go
+++ sql/generarDatos.go
@@ -15,27 +15,27 @@ func cargarDatos() {
 }
 
 
-func cargarClientes() {
-	_ , err = db.Exec(` INSERT INTO cliente VALUES (81635249);
-						INSERT INTO cliente VALUES (97824536);
-						INSERT INTO cliente VALUES (16495823);
-						INSERT INTO cliente VALUES (87512694);
-						INSERT INTO cliente VALUES (58214936);
-						INSERT INTO cliente VALUES (87219364);
-						INSERT INTO cliente VALUES (69254381);
-						INSERT INTO cliente VALUES (24318769);
-						INSERT INTO cliente VALUES (67918245);
-						INSERT INTO cliente VALUES (15624837);
-						INSERT INTO cliente VALUES (93527468);
-						INSERT INTO cliente VALUES (82974315);
-						INSERT INTO cliente VALUES (48129563);
-						INSERT INTO cliente VALUES (65317289);
-						INSERT INTO cliente VALUES (49815267);
-						INSERT INTO cliente VALUES (51362874);
-						INSERT INTO cliente VALUES (83691452);
-						INSERT INTO cliente VALUES (64723591);
-						INSERT INTO cliente VALUES (93167854);
-						INSERT INTO cliente VALUES (84396721);`)
+func cargarClientes() { //FALTA CARGAR TELEFONOS
+	_ , err = db.Exec(` INSERT INTO cliente VALUES (81635249,'Juan','Perez','Corrientes 159',);
+						INSERT INTO cliente VALUES (97824536,'Sergio','Denis','Av Lib San Martín 2130');
+						INSERT INTO cliente VALUES (16495823,'Diego Armando','Maradona','Av Esteco 419');
+						INSERT INTO cliente VALUES (87512694,'Alberto','Fernandez','Ocampo 248');
+						INSERT INTO cliente VALUES (58214936,'Cristina','Kirchner','Av J R Vidal 1751');
+						INSERT INTO cliente VALUES (87219364,'Juan Domingo','Perón','Av Cabildo 2554');
+						INSERT INTO cliente VALUES (69254381,'Nestór','Kirchner','Rioja 951');
+						INSERT INTO cliente VALUES (24318769,'Maximo','Kirchner','Primera Junta 889');
+						INSERT INTO cliente VALUES (67918245,'Mariano','Pavone','Av A Palacios 1545');
+						INSERT INTO cliente VALUES (15624837,'Juan Carlos','Olave','Almirón 301');
+						INSERT INTO cliente VALUES (93527468,'Armando','Paredes','B Mitre 178');
+						INSERT INTO cliente VALUES (82974315,'Juan Roman','Riquelme','E Santamarina 401');
+						INSERT INTO cliente VALUES (48129563,'Martin','Palermo','Av Callao 892');
+						INSERT INTO cliente VALUES (65317289,'Carlos','Tevez','Av San Martín 83');
+						INSERT INTO cliente VALUES (49815267,'Angela Patricia','Gonzales','Alberti 6062');
+						INSERT INTO cliente VALUES (51362874,'Nahuel','Rodriguez','Calle 24 1235');
+						INSERT INTO cliente VALUES (83691452,'Federico','Garcia','Castro Barros 898');
+						INSERT INTO cliente VALUES (64723591,'Matias Adrian','Perez','Río Cuarto 2020');
+						INSERT INTO cliente VALUES (93167854,'Daniela','Gomez','Lavalle 2363');
+						INSERT INTO cliente VALUES (84396721,'Martina','Botero','M De Alzaga 3972');`)
 						
 		if err != nil {
 		log.Fatal(err)
@@ -43,16 +43,16 @@ func cargarClientes() {
 }
 	
 func cargarComercios(){
-	 _ , err = db.Exec(`INSERT INTO comercio VALUES (95169);
-						INSERT INTO comercio VALUES (29981);
-						INSERT INTO comercio VALUES (82211);
-						INSERT INTO comercio VALUES (79701);
-						INSERT INTO comercio VALUES (21724);
-						INSERT INTO comercio VALUES (51249);
-						INSERT INTO comercio VALUES (87682);
-						INSERT INTO comercio VALUES (59460);
-						INSERT INTO comercio VALUES (34039);
-						INSERT INTO comercio VALUES (32694);
+	 _ , err = db.Exec(`INSERT INTO comercio VALUES (95169,'Disco');
+						INSERT INTO comercio VALUES (29981,'Carrefour');
+						INSERT INTO comercio VALUES (82211,'Garbarino');
+						INSERT INTO comercio VALUES (79701,'Coto');
+						INSERT INTO comercio VALUES (21724,'Falabella');
+						INSERT INTO comercio VALUES (51249,'Grido Helado');
+						INSERT INTO comercio VALUES (87682,'Fravega');
+						INSERT INTO comercio VALUES (59460,'Sodimac');
+						INSERT INTO comercio VALUES (34039),'Panaderia y confiteria';
+						INSERT INTO comercio VALUES (32694),'';
 						INSERT INTO comercio VALUES (77289);
 						INSERT INTO comercio VALUES (68463);
 						INSERT INTO comercio VALUES (75374);
