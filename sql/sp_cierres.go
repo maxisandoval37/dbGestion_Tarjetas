8d0aa82efa85e69074a8c4370f78b19fd489d433
Maximiliano Sandoval <maxisandoval98@gmail.com>
2020-12-02 14:07:44 -0300

    update cierres

--- sql/sp_cierres.go
+++ sql/sp_cierres.go
@@ -3,8 +3,8 @@ package sql
 import (
 	"log"
 )
-//store procedure
-func generarCierres() {
+
+func spGenerarCierres() {
 	_, err = db.Query(
 		`CREATE OR REPLACE function generarCierres(anio int) returns void as $$
 		Declare
@@ -15,18 +15,16 @@ func generarCierres() {
 			FOR tarjeta IN 0 .. 9 BY 1
 			LOOP
 				SELECT INTO fdesde to_date((anio - 1)::text || '12' || (select 23 + trunc(random() * 4))::text, 'YYYYMMDD');
-				SELECT INTO fhasta fdesde + cast((select 29 + trunc(random() * 2))::text || ' days' as interval);
-				SELECT INTO fvto fhasta + cast('10 days' as interval);to_date((anio - 1)::text
-
-
+				SELECT INTO fhasta fdesde::date + cast((select 29 + trunc(random() * 2))::text || ' days' as interval);
+				SELECT INTO fvto fhasta::date + cast('10 days' as interval);
+				
 				FOR mes IN 1 .. 12 BY 1
 				LOOP			
-					INSERT INTO cierre VALUES(anio,mes,tarjeta,fdesde,fhasta,fvto);
-					SELECT INTO fdesde fhasta + cast('1 days' as interval);
-					SELECT INTO fhasta fdesde + cast((select 29 + trunc(random() * 2))::text || ' days' as interval);
-					SELECT INTO fvto fhasta + cast('10 days' as interval);
+					INSERT INTO cierre VALUES(anio, mes, tarjeta, fdesde, fhasta, fvto);
+					SELECT INTO fdesde fhasta::date + cast('1 days' as interval);
+					SELECT INTO fhasta fdesde::date + cast((select 29 + trunc(random() * 2))::text || ' days' as interval);
+					SELECT INTO fvto fhasta::date + cast('10 days' as interval);
 				END LOOP;
-
 			END LOOP;
 		END;
 		$$ language plpgsql;`)
