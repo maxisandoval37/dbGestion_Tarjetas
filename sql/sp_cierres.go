9d50663c224e4cfed2c129d57e462864c3ed8b97
nluna <nluna190898@gmail.com>
2020-12-03 15:43:16 -0300

    Corregido sp_cierres

--- sql/sp_cierres.go
+++ sql/sp_cierres.go
@@ -4,26 +4,26 @@ import (
 	"log"
 )
 
-func spGenerarCierres() {
+func SpGenerarCierres() {
 	_, err = db.Query(
 		`CREATE OR REPLACE function generarCierres(anio int) returns void as $$
 		Declare
 			fdesde date;
 			fhasta date;
 			fvto date;
+			anioanterior int;
 		BEGIN
-			FOR tarjeta IN 0 .. 9 BY 1
-			LOOP
-				SELECT INTO fdesde to_date((anio - 1)::text || '12' || (select 23 + trunc(random() * 4))::text, 'YYYYMMDD');
-				SELECT INTO fhasta fdesde::date + cast((select 29 + trunc(random() * 2))::text || ' days' as interval);
-				SELECT INTO fvto fhasta::date + cast('10 days' as interval);
-				
+			anioanterior = (anio-1);
+			FOR ultimo IN 0 .. 9 BY 1
+			LOOP				
 				FOR mes IN 1 .. 12 BY 1
-				LOOP			
-					INSERT INTO cierre VALUES(anio, mes, tarjeta, fdesde, fhasta, fvto);
-					SELECT INTO fdesde fhasta::date + cast('1 days' as interval);
-					SELECT INTO fhasta fdesde::date + cast((select 29 + trunc(random() * 2))::text || ' days' as interval);
-					SELECT INTO fvto fhasta::date + cast('10 days' as interval);
+				LOOP	
+					SELECT INTO fdesde to_date( anioanterior::text || mes, 'YYYYMM' ) 
+						+ CAST(ultimo::text ||' days' AS interval);
+					SELECT INTO fhasta fdesde::date + CAST('1 months' AS interval);
+					SELECT INTO fvto fhasta::date + cast('7 days' as interval);
+					
+					INSERT INTO cierre VALUES(anio, mes, ultimo, fdesde, fhasta, fvto);
 				END LOOP;
 			END LOOP;
 		END;
