39c6c7f2c1e53f4b8ac0372ef5de16acb41f4b88
nluna <nluna190898@gmail.com>
2020-12-06 17:10:40 -0300

    UPDATE CLI

--- main.go
+++ main.go
@@ -11,77 +11,100 @@ func main() {
 	menuPrincipal()
 }
 
-var opcion int=0
+var opcion int = -1
 
 func menuPrincipal() {
-
-		for opcion != 99 {
-			fmt.Println("------------------")
-			fmt.Println("Digite una opcion")
-			fmt.Println("------------------")
-			fmt.Println("1: Crear BD SQL")
-			fmt.Println("2: Crear Tablas")
-			fmt.Println("3: Mostrar Tablas de la BD")
-			fmt.Println("4: Generar PKs y FKs")
-			fmt.Println("5: Borrar PKs y FKs")
-			fmt.Println("6: Cargar datos")
-			fmt.Println("7: Mostrar Todos Datos Cargados")
-			fmt.Println("8: Generar Cierres")
-			fmt.Println("9: Agregar Consumos")
-			
-			fmt.Println("96: Alertas")
-			
-			fmt.Println("97: Generar Resumenes")
-			fmt.Println("98: CargarDatosNoSQL en BoltDB")
-			fmt.Println("99: Salir ")
-			fmt.Scanf("%d", &opcion)
+	
+	for opcion != 0 {
+		fmt.Println("-------------------------------------")
+		fmt.Println("------- - DIGITE UNA OPCION - -------")
+		fmt.Println("-------------------------------------")
+		fmt.Println("-  0:  .    .    .    .    .  SALIR -")
+		fmt.Println("-  1:   .    .    .    Crear BD SQL -")
+		fmt.Println("-  2:    .    .    .   Crear Tablas -")
+		fmt.Println("-  3:.    .    .  Generar PKs y FKs -")
+		fmt.Println("-  4: .    .    .    .    . Alertas -")
+		fmt.Println("-  5:  .    .Generar Cierres (2020) -")
+		fmt.Println("-  6:   .    .    .    Cargar datos -")
+		fmt.Println("-  7:  .    .    . Agregar Consumos -")
+		fmt.Println("-  8: .    .    . Generar Resumenes -")
+		fmt.Println("-------------------------------------")
+		fmt.Println("-  9: .     Mostrar Tablas de la BD -")
+		fmt.Println("- 10:  Mostrar Todos Datos Cargados -")	
+		fmt.Println("- 11:    .    .    Borrar PKs y FKs -")		
+		fmt.Println("- 12:.   CargarDatosNoSQL en BoltDB -")
+		fmt.Println("-------------------------------------")		
+		fmt.Scanf("%d", &opcion)
 		
-		switch {
-			case opcion == 1:
-				sql.CrearDB()
-				fmt.Println("--------------------\n    ¡BD CREADA!\n--------------------")
-			case opcion == 2:
-				sql.CrearTablas()
-				fmt.Println("------------------------\n    ¡TABLAS CREADAS!\n------------------------")
-			case opcion == 3:
-				sql.MostrarTablas()
-				fmt.Println("-----------------------\n    ¡TABLAS MOSTRADAS!\n-----------------------")
-			case opcion == 4:
-				sql.GenerarPKs()
-				sql.GenerarFKs()
-				fmt.Println("-----------------------\n    ¡KEYS GENERADAS!\n-----------------------")
-			case opcion == 5:
-				sql.BorrarFKs()
-				sql.BorrarPKs()
-				fmt.Println("-----------------------\n    ¡KEYS BORRADAS!\n----------------------")
-			case opcion == 6:
-				sql.CargarDatos()
-				fmt.Println("-----------------------\n    ¡DATOS CARGADOS!\n-----------------------")
-			case opcion == 7:
-				sql.MostrarTodosDatos()
-				fmt.Println("---------------------------\n    ¡DATOS MOSTRADOS!\n-------------------------")
-			case opcion == 8:
-				sql.Cierres()
-				fmt.Println("---------------------------\n    ¡DATOS CIERRES GENERADOS\n---------------------------")
-			case opcion == 9:
-				sql.TestConsumo()
-				fmt.Println("-----------------------------------------\n    ¡CONSUMOS AGREGADOS Y GESTIONADOS!\n-----------------------------------------")
-
-			case opcion == 96:
-				sql.Triggers()
-				fmt.Println("-------------------------\n    ¡GENERAR ALERTAS!\n-------------------------")
-
-
-
-
-			case opcion == 97:
-				sql.GenerarResumenesPrincipal()
-				fmt.Println("---------------------------\n    ¡RESUMENES GENERADOS!\n---------------------------")
-			case opcion == 98:
-				nosql.CargaDatosNoDB()
-				fmt.Println("---------------------------\n    ¡DATOS NOSQL CARGADOS!\n---------------------------")
-			default:
-				fmt.Println("Ingrese una opcion valida")
-			}
+	switch {
+		case opcion == 0:
+			fmt.Println("-------------------------------------")
+			fmt.Println("------------ ¡HA SALIDO! ------------")
+			fmt.Println("-------------------------------------")
+		case opcion == 1:
+			sql.CrearDB()
+			fmt.Println("-------------------------------------")
+			fmt.Println("------------ ¡BD CREADA! ------------")
+			fmt.Println("-------------------------------------")			
+		case opcion == 2:
+			sql.CrearTablas()
+			fmt.Println("-------------------------------------")
+			fmt.Println("--------- ¡TABLAS  CREADAS! ---------")
+			fmt.Println("-------------------------------------")		
+		case opcion == 3:
+			sql.GenerarPKs()
+			sql.GenerarFKs()
+			fmt.Println("--------- ¡KEYS  GENERADAS! ---------")
+			fmt.Println("-------------------------------------")		
+		case opcion == 4:
+			sql.Triggers()
+			fmt.Println("-------------------------------------")
+			fmt.Println("--------- ¡GENERAR ALERTAS! ---------")
+			fmt.Println("-------------------------------------")
+		case opcion == 5:
+			sql.Cierres()
+			fmt.Println("-------------------------------------")
+			fmt.Println("-------- ¡CIERRES GENERADOS! --------")
+			fmt.Println("-------------------------------------")
+		case opcion == 6:
+			sql.CargarDatos()
+			fmt.Println("-------------------------------------")
+			fmt.Println("--------- ¡DATOS  CARGADOS! ---------")
+			fmt.Println("-------------------------------------")		
+		case opcion == 7:
+			sql.TestConsumo()
+			fmt.Println("-------------------------------------")
+			fmt.Println("------- ¡CONSUMOS  AGREGADOS! -------")
+			fmt.Println("-------------------------------------")
+		case opcion == 8:
+			sql.GenerarResumenesPrincipal()
+			fmt.Println("-------------------------------------")
+			fmt.Println("------- ¡RESUMENES GENERADOS! -------")
+			fmt.Println("-------------------------------------")
+		
+		case opcion == 9:
+			sql.MostrarTablas()
+			fmt.Println("-------------------------------------")
+			fmt.Println("-------- ¡TABLAS  MOSTRADAS! --------")
+			fmt.Println("-------------------------------------")
+		case opcion == 10:
+			sql.MostrarTodosDatos()
+			fmt.Println("-------------------------------------")
+			fmt.Println("--------- ¡DATOS MOSTRADOS! ---------")
+			fmt.Println("-------------------------------------")
+		case opcion == 11:
+			sql.BorrarFKs()
+			sql.BorrarPKs()
+			fmt.Println("-------------------------------------")
+			fmt.Println("---------- ¡KEYS BORRADAS! ----------")
+			fmt.Println("-------------------------------------")
+		case opcion == 12:
+			nosql.CargaDatosNoDB()
+			fmt.Println("-------------------------------------")
+			fmt.Println("------ ¡DATOS NO-SQL CARGADOS! ------")
+			fmt.Println("-------------------------------------")
+		default:
+			fmt.Println("Ingrese una opcion valida")
+		}
 	}
 }
